2d727e1181d0c8701b622ae0aa68f72e10b58725
Dr4gnSt4lk <homesasha1@gmail.com>
2024-01-15 15:37:25 +0200

    Убрал лишнее

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_build/app_navigation.dart';
-import 'package:shop_build/mainwrapper.dart';
-import 'package:shop_build/screens/home/homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shop_build/screens/search/searchpage.dart';
-import 'package:shop_build/screens/search/tags.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,10 +17,6 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
           textTheme:
               GoogleFonts.montserratTextTheme(Theme.of(context).textTheme)),
-      //home: const HomePage(),
-      //home: const SettingsPage(),
-      //home: const SearchPage(),
-      //home: const TagsPage(),
       routerConfig: AppNavigation.router,
     );
   }
